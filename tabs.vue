3d5a081ebe6039bfbe046b9d75bd329ead44df90
g_rome <Gal.Roma@afp.com>
2018-01-18 17:01:48 +0800

    first commit

---
+++ tabs.vue
@@ -0,0 +1,56 @@
+<template>
+  <div>
+    <nav>
+      <ul>
+        <li v-for="item,i in items" v-on:click="switchItem(i)" :class="{active: i==current}" :style="{backgroundColor: item.props.color}">
+        {{ item.props.name }}
+        </li>
+      </ul>
+    </nav>
+    <main>
+      <img :src="currentImage" />
+      <h3>{{ currentItem.props.title }}</h3>
+      <h4>{{ currentItem.props.date }}</h3>
+      <h2>{{ currentItem.props.toll }}</h2>
+      <p> {{ currentItem.props.description }}</p>
+
+
+    </main>
+    
+  </div> 
+</template>
+
+<script>
+  module.exports = {
+    data() {
+      return {
+        current: 0,
+        size: null
+      }
+    },
+    props: ['items'],
+    computed: {
+      currentItem() {
+        return this.items[this.current]
+      },
+      currentImageSize() {
+        return 'image'
+      },
+      currentImage() {
+        return 'assets/' + this.currentItem[this.currentImageSize]
+      }
+    },
+    mounted() {
+      this.resize()
+      window.onresize = this.resize
+    },
+    methods: {
+      resize() {
+        this.size = this.$el.getBoundingClientRect()
+      },
+      switchItem(i) {
+        this.current = i
+      }
+    }
+  }
+</script>
